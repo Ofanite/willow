d4787b9b36ca6b83b667da1bb0f4821155a16baf
Adam Bell <adam.bell@viximo.com>
2012-01-21 12:52:13 -0500

    Allright, it's a new rails app!  Rad!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Willow::Application.config.secret_token = 'a2fc94f17a6eb07a85e90b4f8fc1d63e2256b8333b49ab1048d6ebb63d9e7d77561fd6894c9420cf5861b0c865218da770f617b403c9eff68d2a48ea0e3ca938'
