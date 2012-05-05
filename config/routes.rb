d658460e7e1aa4f6abd62c3d97becf5b2434f845
Adam Bell <adam.bell@viximo.com>
2012-05-05 13:28:32 -0400

    More migrations.

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Willow::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "welcome#index"
+  root :to => "home#index"
 
   devise_for :users
 
