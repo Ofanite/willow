82c6f10144b3c5ef832e6da13eb93df2aa95b029
Adam Bell <adam.bell@viximo.com>
2012-11-03 11:36:41 -0400

    Adding a basic model for characters - we can expand it later.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120505165759) do
+ActiveRecord::Schema.define(:version => 20121103152409) do
+
+  create_table "characters", :force => true do |t|
+    t.text     "stats"
+    t.text     "description"
+    t.string   "name"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "entries", :force => true do |t|
     t.string   "title"
